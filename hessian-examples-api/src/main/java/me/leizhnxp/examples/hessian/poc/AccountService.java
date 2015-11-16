641f0a339cee4805b0f618e4a4078920154dadf7
leizhnxp <leizhnxp@gmail.com>
2015-11-17 07:52:11 +0800

    sample api code

---
+++ hessian-examples-api/src/main/java/me/leizhnxp/examples/hessian/poc/AccountService.java
@@ -0,0 +1,9 @@
+package me.leizhnxp.examples.hessian.poc;
+
+import java.util.List;
+
+public interface AccountService {
+    public void insertAccount(Account account);
+
+    public List<Account> getAccounts(String name);
+}
